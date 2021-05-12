81e6688543998b83c5056a579f4f9b7805ee0d6b
GuilhermeDanzer <guidanzer@hotmail.com>
2021-05-12 05:03:52 -0300

    minor change

--- src/components/Texts.tsx
+++ src/components/Texts.tsx
@@ -1,15 +1,14 @@
-
 import styled from "styled-components";
 import { TextsProps } from "../interfaces/componentsInterface";
 
 export const Titulo = styled.h1<TextsProps>`
   color: #fff;
   font-weight: 700;
-  font-size: ${(p) => (p.size ? p.size : "30px")};
+  font-size: ${(props) => (props.size ? props.size : "30px")};
 `;
 export const Text = styled.p<TextsProps>`
   color: #fff;
   font-weight: 700;
   font-family: "Montserrat", sans-serif;
-  font-size: ${(p) => (p.size ? p.size : "16px")};
+  font-size: ${(props) => (props.size ? props.size : "16px")};
 `;
